f168a4d7c2af7470898dd8455c3ea8d16670044b
Khushi Sinha <khushisinha4786@gmail.com>
2026-01-02 22:19:02 +0530

    Initial Spring Boot setup with test endpoint

---
+++ src/main/java/com/goalflow/goalflow_api/GoalflowApiApplication.java
@@ -0,0 +1,13 @@
+package com.goalflow.goalflow_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalflowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoalflowApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goalflow/goalflow_api/GoalflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goalflow.goalflow_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalflowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
